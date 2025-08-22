6e2e9e0a594fa0d6627e1c8a147dcdbaaba48c91
alvarogonmar <alvarogonmar0@gmail.com>
2025-08-22 12:34:23 -0600

    fix: move maxArea initialization inside containerWithMostWater function

--- ContainerWithMostWater.py
+++ ContainerWithMostWater.py
@@ -2,6 +2,7 @@
 def containerWithMostWater(array, out, height): # def containerWithMostWater para encontrar el contenedor con más agua
         l = 0
         r = len(height) - 1
+        maxArea = 0
 
 
 
