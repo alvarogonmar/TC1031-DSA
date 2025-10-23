b8a30777e012126cb1395fc9f7326c1529fc8011
alvarogonmar <alvarogonmar0@gmail.com>
2025-10-23 06:55:41 -0600

    fix: initialize hash variable in HashTable::hash function

--- Act2-HashTable/HashTable.cpp
+++ Act2-HashTable/HashTable.cpp
@@ -7,3 +7,5 @@ HashTable::Entry::Entry() : key(""), value(0), occupied(false), deleted(false) {
 
 // Hash function: polynomial rolling hash
 int HashTable::hash(const std::string &key) const
+{
+    unsigned long hash = 0;
