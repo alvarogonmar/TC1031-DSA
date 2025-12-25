d0414248217002f2ac42a11f11549d23a65472ff
alvarogonmar <alvarogonmar0@gmail.com>
2025-12-25 11:24:02 -0600

    feat: add console log for delete value test in LinkedList

--- LinkedList/linklist.js
+++ LinkedList/linklist.js
@@ -161,3 +161,4 @@ console.log('Search 100:', myList.search(100)); // true
 console.log('Search 999:', myList.search(999)); // false
 
 console.log('\n== Delete value 200 (middle) ==');
+myList.deleteValue(200);
